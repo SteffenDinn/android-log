067b7b9c55cdcfeedeab512cdf9838d3b2febcbc
SteffenDinn <steffen.dinn@qq.com>
2019-09-20 09:03:11 +0800

    新增 Application

---
+++ app/src/main/java/net/dinn/gitee/application/DApplication.java
@@ -0,0 +1,14 @@
+package net.dinn.gitee.application;
+
+import android.app.Application;
+
+/**
+ * @Description: <li/>Created by SteffenDinn on 2019/9/20.
+ * <li/>请求参数(Bundle):
+ * <li/>
+ * <p/>返回状态码: result = ?
+ * <li/>返回参数(Bundle):
+ * <li/>
+ */
+public class DApplication extends Application {
+}
